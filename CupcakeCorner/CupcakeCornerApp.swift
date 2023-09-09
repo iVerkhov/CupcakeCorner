407b62a59beb6cbe1826b87046886f592e3cc069
iVerkhov <vrkhv@iCloud.com>
2023-09-09 13:20:27 +0300

    Initial Commit

---
+++ CupcakeCorner/CupcakeCornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CupcakeCornerApp.swift
+//  CupcakeCorner
+//
+//  Created by Игорь Верхов on 09.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct CupcakeCornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
